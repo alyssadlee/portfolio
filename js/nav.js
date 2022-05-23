1a20f0780553cfade47edcb7e6dbd416e14d61ca
alyssadlee <alyssadlee@gmail.com>
2022-05-22 23:08:20 -0700

    fixed nav buttong toggle

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,21 @@
-const getPrimaryNav = document.getElementById('#primary-navigation');
+const primaryNav = document.querySelector("#primary-navigation");
+const navToggle = document.querySelector(
+  "[aria-controls='primary-navigation']"
+);
 
-function primaryNavToggle() {
-    if (primaryNavToggle.style.display === "none") {
-        primaryNavToggle.style.display === "block"; 
-    } else {
-        primaryNavToggle.style.display === "none";
-    }
-}
+navToggle.addEventListener("click", () => {
+  const visibility = primaryNav.getAttribute("data-visible");
+
+  if (visibility === "false") {
+    primaryNav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+  } else if (visibility === "true") {
+    primaryNav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+  }
+});
+
+
 
 
 
