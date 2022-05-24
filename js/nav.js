df3221c76fcc0f4038ef13a3385e41b408a9201b
alyssadlee <alyssadlee@gmail.com>
2022-05-23 18:12:32 -0700

    fixed responsive nav bar

--- js/nav.js
+++ js/nav.js
@@ -3,36 +3,49 @@ const navToggle = document.querySelector(
   "[aria-controls='primary-navigation']"
 );
 
-navToggle.addEventListener("click", () => {
-  const visibility = primaryNav.getAttribute("data-visible");
+function toggleMenu() {
+  const visibility = primaryNav.getAttribute('data-visible');
+
+  if (!visibility) {
+    console.warn("Primary nav data attribute : data-visible not found");
+    return;
+  }
 
   if (visibility === "false") {
     primaryNav.setAttribute("data-visible", true);
     navToggle.setAttribute("aria-expanded", true);
-  } else if (visibility === "true") {
-    primaryNav.setAttribute("data-visible", false);
-    navToggle.setAttribute("aria-expanded", false);
+    return
   }
-});
 
+  primaryNav.setAttribute("data-visible", false);
+  navToggle.setAttribute("data-visible", false);
+  return;
+}
 
-function UseClickOutside(domRef, _cb, event, ...truthyChecks) {
+function UseClickOutside() {
+  const primaryNavIsVisible = primaryNav.getAttribute("data-visible") === "true";
   // Make sure that the menu is Open before running the rest of the function
-      if (!truthyChecks.every(Boolean)) {
+      if (!primaryNavIsVisible) {
         return;
       }
   // check the click target
-        const visibility = event.target;
+  const target = event.target;
   
   // if the click target is not a child of the #primary-navigation, then we run the Callback
         if (!primaryNav.contains(target)) {
-          _cb();
+          toggleMenu();
+          return;
+        }
+
+        const targetClassContainsNavLink = [...target.classList].find((className) => className === "navigation_link");
+        if (targetClassContainsNavLink) {
+          toggleMenu();
           return;
         }
         return;
       }
   
-document.addEventListener('mousedown', (event) => UseClickOutside('#primary-navigation', toggleMenu, event, primaryNav.getAttribute("data-visible") ), true);
+document.addEventListener('mousedown', UseClickOutside, true);
 
 
 
