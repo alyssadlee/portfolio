20457803d9b5727abc51055a8fcec936e28c1442
alyssadlee <alyssadlee@gmail.com>
2022-05-26 23:26:22 -0700

    contact form

--- js/form.js
+++ js/form.js
@@ -1,15 +1,15 @@
 function sendEmail(){
-    Email.send({
-        Host : "smtp.gmail.com",
-        Username : "alyssadlee@gmail.com",
-        Password : "ynfmjmkeuoeufzyh",
-        To : 'alyssadlee@gmail.com',
-        From : document.getElementById("email").value,
-        Subject : "New Contact Form Enquiry",
-        Body : "Name: " + document.getElementById("name").value
-            + "<br> Email: " + document.getElementById("email").value 
-            + "<br> Message: " + document.getElementById("message").value
-    }).then(
-      message => alert("Message Sent Successfully")
-    );
+  Email.send({
+      Host : "smtp.gmail.com",
+      Username : "alyssadlee@gmail.com",
+      Password : "lcjrfruaqmkdfvhr",
+      To : 'alyssadlee@gmail.com',
+      From : document.getElementById("email").value,
+      Subject : "New Contact Form Enquiry",
+      Body : "Name: " + document.getElementById("name").value
+          + "<br> Email: " + document.getElementById("email").value 
+          + "<br> Message: " + document.getElementById("message").value
+  }).then(
+    message => alert("Sorry! Form under construction :/ click icons in footer for socials")
+  );
 }
