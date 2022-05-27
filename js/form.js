5048dee80ea2880b63ea7ae3ad519f29d6917694
alyssadlee <alyssadlee@gmail.com>
2022-05-26 22:24:33 -0700

    contact page

---
+++ js/form.js
@@ -0,0 +1,15 @@
+function sendEmail(){
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "alyssadlee@gmail.com",
+        Password : "ynfmjmkeuoeufzyh",
+        To : 'alyssadlee@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : "New Contact Form Enquiry",
+        Body : "Name: " + document.getElementById("name").value
+            + "<br> Email: " + document.getElementById("email").value 
+            + "<br> Message: " + document.getElementById("message").value
+    }).then(
+      message => alert("Message Sent Successfully")
+    );
+}
